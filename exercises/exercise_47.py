c07fbedbff6e4d937295cd49219c583f1f30bd97
kw4dw0 <152057939+kw4dw0@users.noreply.github.com>
2024-04-08 16:06:15 +0100

    Update exercise_47.py

--- exercises/exercise_47.py
+++ exercises/exercise_47.py
@@ -1,1 +1,14 @@
 # Your solution to Exercise 47
+verif = False
+while not verif:
+  n = int(input())
+  if (1 <= n <= 100000):
+    verif = True
+
+for num in range(1, n+1):
+  num = str(num)
+  sum = 0
+  for j in num:
+    sum += int(j)
+  if sum == int(num[0]) * len(num):
+    print(num, end = " ")
